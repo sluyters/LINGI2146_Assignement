dd5fd5f71b2a6b71851f8f856ad8515cb2283d75
Arthur Sluyters <sluytersarthur@gmail.com>
2019-04-12 19:33:55 +0200

    Modifications to the code

--- sensor_network/sensor.c
+++ sensor_network/sensor.c
@@ -11,14 +11,39 @@
 //#define DEBUG DEBUG_PRINT
 
 #include "net/rime/rime.h"	
+/*-----------------------------------------------------------------------------*/
+/* Configuration values */
+const uint16_t runicast_channel = 142;
+const uint16_t broadcast_channel = 169;
+const uint8_t version = 1;
+
+struct runicast_conn runicast;
+struct broadcast_conn broadcast;
 
 /*-----------------------------------------------------------------------------*/
-/* Information about a neighboring node */
-struct neighbor_node {
-	struct neighbor_node *next;
-	linkaddr_t addr;
-	// TODO 
+/* Structure of the messages */
+enum {
+	DESTINATION_ADVERTISEMENT, 
+	TREE_ADVERTISEMENT, 
+	TREE_INFORMATION_REQUEST
+};
+
+struct message {
+	uint8_t version;
+	uint8_t msg_type;
+	uint8
 }
+
+/*-----------------------------------------------------------------------------*/
+/* Save parent + child nodes */
+struct node {
+	struct node *next;
+	rimeaddr_t addr;
+	uint8_t n_hops;
+}
+
+struct node *parent;
+struct node *childs;
 
 /*-----------------------------------------------------------------------------*/
 /* Declaration of the processes */
@@ -27,9 +52,33 @@ PROCESS(broadcast_process, "Broadcast process");
 AUTOSTART_PROCESSES(&broadcast_process);
 
 /*-----------------------------------------------------------------------------*/
+/* Helper functions */
+static void encode_message(struct message *decoded_msg, char *encoded_msg) {
+}
+
+static void decode_message(struct message *decoded_msg, char *encoded_msg) {
+}
+
+/*-----------------------------------------------------------------------------*/
 /* Callback function when a broadcast message is received */
 static void broadcast_recv(struct broadcast_conn *c, const rimeaddr_t *from) {
-	// TODO
+	if (parent == NULL) {
+		// TODO Check if the new parent is not a child + check if malloc OK
+		parent = (struct node *) malloc(sizeof(struct node));
+		parent.addr = *from;		// Not sure
+		parent.next = NULL;
+		parent.n_hops = 
+		// Send a DESTINATION_ADVERTISEMENT message to the new parent node
+		struct message msg;
+		msg.version = version;
+		msg.type = DESTINATION_ADVERTISEMENT;
+		char *encoded_msg;
+		encode_message(msg, encoded_msg);
+		packetbuf_copyfrom(encoded_msg, sizeof(encoded_msg));	// Put data inside the packet
+		runicast_send(&runicast, from, 1);
+	} else {	
+	
+	}
 }
 
 // Set the function to be called when a broadcast message is received
@@ -43,6 +92,8 @@ static void runicast_recv(struct runicast_conn *c, const rimeaddr_t *from) {
 
 // Set the function to be called when a broadcast message is received
 static const struct runicast_callbacks rc = {runicast_recv}
+
+// TODO Open unicast -> runicast_open();
 
 /*-----------------------------------------------------------------------------*/
 /* Process */
